8171a61998a98568179dc2039bade6d73d940dcf
Vagabond K <vagabond-k@naver.com>
2021-11-12 21:41:41 +0900

    네임스페이스 주석 추가

---
+++ VagabondK.Korean/NamespaceDoc.cs
@@ -0,0 +1,65 @@
+﻿namespace VagabondK.Korean
+{
+    /// <summary>
+    /// 한국에 국한된 여러 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceGroupDoc
+    {
+    }
+}
+
+namespace VagabondK.Korean
+{
+    /// <summary>
+    /// 한국에 국한된 여러 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceDoc
+    {
+    }
+}
+
+namespace VagabondK.Korean.Hangul
+{
+    /// <summary>
+    /// 한글과 관련된 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceGroupDoc
+    {
+    }
+}
+
+namespace VagabondK.Korean.Hangul
+{
+    /// <summary>
+    /// 한글과 관련된 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceDoc
+    {
+    }
+}
+
+namespace VagabondK.Korean.ScreenInput
+{
+    /// <summary>
+    /// 한국어의 화면 기반 입력 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceGroupDoc
+    {
+    }
+}
+
+namespace VagabondK.Korean.ScreenInput
+{
+    /// <summary>
+    /// 한국어의 화면 기반 입력 기능들을 지원하는 라이브러리입니다.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated()]
+    class NamespaceDoc
+    {
+    }
+}
